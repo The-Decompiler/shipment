efdd93a595d3d4b8dea1ba3b3e0527f0b65e8aad
slow typer <slow@slowtyper.com>
2021-04-03 12:42:35 +0300

    Add error messages and filesize limit.

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -8,12 +8,19 @@ type Props = {
 export const FileUpload = (props: Props) => {
 	const [loadFile, setLoadFile] = useState(0);
 	const [fileUrl, setFileUrl] = useState("");
+	const [error, setError] = useState("");
 
 	useEffect(() => {
+		if (props.file.size > 10 * 1024 * 1024) {
+			setError("File exceeds the upload limit (10MB)");
+			return
+		}
+
 		const uploadTask = storage.ref(`files/${props.file.name}`).put(props.file);
 		uploadTask.on("state_changed", snapshot => {
 			setLoadFile(Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100));
 		}, error => {
+			setError("Something went wrong...")
 			console.log(error);
 		}, () => {
 			storage.ref("files")
@@ -31,6 +38,7 @@ export const FileUpload = (props: Props) => {
 			<p>File Size: {props.file.size}</p>
 			<p>{loadFile}%</p>
 			<p>{fileUrl}</p>
+			<p>ERROR: {error}</p>
 		</div>
 	)
 }
