fd903acfbabcedf116570b9d4bf3e87e1ca34f15
maxminoS <maxminos.1@gmail.com>
2021-04-05 00:10:04 +0700

    Added custom theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
+			colors: {
+				gray: {
+					"bg": "#ccccccff",
+					"hex": "#9b9393ff"
+				},
+				btn: {
+					"primary": "#ff5555ff",
+					"secondary": "#1e0102ff",
+					"hover": "#aa0000ff",
+					"active": "#1e0102ff"
+				}
+			},
 			fontFamily: {
 				sans: ["Poppins", "Arial", "sans-serif"],
 			},
