29a6d62c0e26795bb5cd7f680f881c3789ac83d0
maxminoS <maxminos.1@gmail.com>
2021-04-04 21:54:51 +0700

    Styled responsive DragAndDrop component

--- src/components/DragAndDrop.tsx
+++ src/components/DragAndDrop.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+
+import hexagon from "../static/hexagon.png";
+import box1 from "../static/box1.gif";
 
 type Props = {
 	setUploads: React.Dispatch<React.SetStateAction<File[] | null>>,
 }
 
 export const DragAndDrop = (props: Props) => {
+	const [dragging, setDragging] = useState(false);
+
 	const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
 		e.preventDefault();
 		e.stopPropagation();
@@ -16,19 +21,29 @@ export const DragAndDrop = (props: Props) => {
 	const handleDragEnter = (e: React.DragEvent<HTMLDivElement>) => {
 		e.preventDefault();
 		e.stopPropagation();
+		setDragging(true);
 	}
 
-	const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+	const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
 		e.preventDefault();
+		setDragging(false);
 	};
 
 	return (
-		<div className="h-24 bg-gray-200"
-			onDrop={handleDrop}
-			onDragEnter={handleDragEnter}
-			onDragOver={handleDragOver}
-		>
-			<p>Drag files here</p>
-		</div>
+		<>
+			<div className={"invisible md:visible absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-2/3 select-none z-20 " + (dragging && "scale-105")}
+				onDrop={handleDrop}
+				onDragEnter={handleDragEnter}
+				onDragLeave={handleDragLeave}
+			>
+				<img src={hexagon} alt="Drop your files here" />
+				<p className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-20
+					w-60 text-center text-4xl text-gray-hex">
+					Drop your files here
+				</p>
+			</div>
+			<img className="absolute top-1/4 md:top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-8 select-none z-20"
+			src={box1} alt="Drop box" />
+		</>
 	)
 }
