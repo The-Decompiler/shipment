7bb11b56e60aae3be98950ce592b0625ccd86118
slow typer <slow@slowtyper.com>
2021-04-05 08:05:26 +0300

    Add semicolons...

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,8 @@
 export const prettyBytes = (bytes: number) => {
-    const units = ['B', 'KB', 'MB']
+    const units = ['B', 'KB', 'MB'];
     for (const unit of units) {
-	    console.log(unit)
-        if (bytes < 1000) return `${bytes.toFixed(2)}${unit}`
-        bytes /= 1000
+        if (bytes < 1000) return `${bytes.toFixed(2)}${unit}`;
+        bytes /= 1000;
     }
-    return `${bytes.toFixed(2)}GB`
+    return `${bytes.toFixed(2)}GB`;
 }
